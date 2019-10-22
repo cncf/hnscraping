b2f3164c9f9da22fd80079397eee2a299f8ab003
Jordi Noguera <jordinoguera83+github@gmail.com>
2019-10-22 17:18:59 +0200

    Uncomment line

--- lib/app.rb
+++ lib/app.rb
@@ -19,6 +19,6 @@ class App < Sinatra::Base
 
   def verify_signature(payload_body)
     signature = 'sha1=' + OpenSSL::HMAC.hexdigest(OpenSSL::Digest.new('sha1'), ENV.fetch('GITHUB_TOKEN'), payload_body)
-    # return halt 500, "Signatures didn't match!" unless Rack::Utils.secure_compare(signature, request.env['HTTP_X_HUB_SIGNATURE'])
+    return halt 500, "Signatures didn't match!" unless Rack::Utils.secure_compare(signature, request.env['HTTP_X_HUB_SIGNATURE'])
   end
 end
